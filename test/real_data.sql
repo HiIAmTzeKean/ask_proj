af417b31c877f03e8ed44715eec6dd45c4958232
HiIAmTzeKean <ngtzekean600@gmail.com>
2021-02-26 01:03:03 +0800

    testing with hello page

--- test/real_data.sql
+++ test/real_data.sql
@@ -23,5 +23,23 @@ INSERT INTO instructor
 
 INSERT INTO dojo
   (id,name,location,instructor_id)
-  VALUES
+VALUES
   (0,'NSCC','Nee Soon Central CC', 0);
+
+INSERT INTO enrollment
+("studentActive",student_id, dojo_id)
+VALUES
+('True',0,0),
+('True',1,0),
+('True',2,0),
+('True',3,0),
+('True',4,0),
+('True',5,0),
+('True',6,0),
+('True',7,0),
+('True',8,0),
+('True',9,0),
+('True',10,0),
+('True',11,0),
+('True',12,0),
+('True',13,0);
