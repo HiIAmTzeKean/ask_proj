1f9c37d7f92802ad62566fa3a103b7f1b831d3fe
HiIAmTzeKean <ngtzekean600@gmail.com>
2021-03-18 21:42:21 +0800

    added delete student option

--- flaskapp/instructor/instructor.py
+++ flaskapp/instructor/instructor.py
@@ -5,7 +5,7 @@ from flask import (Blueprint, flash, g, make_response, redirect,
                    render_template, request, session, url_for)
 from flaskapp import db
 from flaskapp.models import dojo, enrollment, lesson, student, studentStatus, instructor
-from flaskapp.instructor.form import formEditInstructor
+from flaskapp.instructor.form import formEditInstructor, formSearchStudent
 
 instructor_bp = Blueprint('instructor', __name__,
                            template_folder='templates', static_folder='static')
@@ -17,6 +17,7 @@ def instructorViewer():
     instructor_list = db.session.query(instructor).all()
     return render_template('instructor/instructorViewer.html',
                            instructor_list=instructor_list)
+
 
 #todo show classes in this page as well
 @instructor_bp.route('/instructorEditInstructor/<instructor_id>', methods=('GET', 'POST'))
@@ -30,6 +31,15 @@ def instructorEditInstructor(instructor_id):
         return redirect(url_for('instructor.instructorEditInstructor', instructor_id=instructor_id)) # return back same view page
     return render_template('instructor/instructorEditInstructor.html', instructorRecord=instructorRecord,form=form)
 
+
+#todo delete student tab
+@instructor_bp.route('/instructorDeleteStudent/<student_id>', methods=('GET', 'POST'))
+def instructorDeleteStudent(student_id):
+    studentRecord = db.session.query(student).filter(student.id==student_id).first()
+    db.session.delete(studentRecord)
+    db.session.commit()
+    return redirect(url_for('instructor.instructorSearchStudent'))
+
 #todo
 @instructor_bp.route('/instructorSearchStudent', methods=('GET', 'POST'))
 def instructorSearchStudent():
@@ -39,7 +49,10 @@ def instructorSearchStudent():
         serachBelt = request.args.get('serachBelt')
         student_list = db.session.query(student).filter(student.name.ilike('%{}%'.format(serachString))).all() # case insensitive
     else:
-        student_list = db.session.query(student).all()
+        # only query student records and not instr
+        instructor_list = db.session.query(instructor.id).all()
+        student_list = db.session.query(student).filter(student.id.notin_(instructor_list)).all()
+
     if form.validate_on_submit():
         serachString = form.name.data
         serachBelt = form.belt.data
