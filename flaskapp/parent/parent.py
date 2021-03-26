716021337c6b77d132103024d6e5524864139f29
HiIAmTzeKean <ngtzekean600@gmail.com>
2021-03-26 23:33:43 +0800

    decode fix

--- flaskapp/parent/parent.py
+++ flaskapp/parent/parent.py
@@ -47,12 +47,13 @@ def parentChartView(studentID, template):
                            spirit=spirit, dateLabel=dateLabel, countdown=countdown,
                            lessonDone=lessonDone, myRemarks=myRemarks)
 
+
 def messageEncode(message):
     message_bytes = message.encode('ascii')
     base64_bytes = base64.b64encode(message_bytes)
     return base64_bytes.decode('ascii')
 
 def messageDecode(message):
-    base64_bytes = base64_message.encode('ascii')
+    base64_bytes = message.encode('ascii')
     message_bytes = base64.b64decode(base64_bytes)
     return int(message_bytes.decode('ascii'))
